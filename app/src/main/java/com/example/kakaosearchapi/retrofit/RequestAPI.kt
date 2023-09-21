f32fc365c24b54ed3d18ef001ac2b55d8383db8c
iWantBigMan <skarndgus8899@gmail.com>
2023-09-21 19:59:03 +0900

    [FEAT] : ⚙️Data Reponse Interface

---
+++ app/src/main/java/com/example/kakaosearchapi/retrofit/RequestAPI.kt
@@ -0,0 +1,20 @@
+package com.example.kakaosearchapi.retrofit
+
+
+import com.example.kakaosearchapi.constants.Constants
+import com.example.kakaosearchapi.model.SearchResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface RequestAPI {
+    @GET("v2/search/image")
+    suspend fun searchImage(
+        @Header("Authorization") apiKey: String = Constants.AUTH_HEADER,
+        @Query("query") query : String,
+        @Query("sort") sort : String,
+        @Query("page") page: Int,
+        @Query("size") size: Int
+    ) : Response<SearchResponse>
+}
